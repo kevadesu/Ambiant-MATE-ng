63a14ead553772142f8adf5ad428bc7cf2ba5a64
Brotmetchaes · kevadesu <76264518+kevadesu@users.noreply.github.com>
2024-12-15 18:44:20 +0100

    Update installer.sh
    Add error handling(?)

--- installer.sh
+++ installer.sh
@@ -7,15 +7,23 @@ fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "[1/5] Installing themes..."
-cp $SCRIPT_DIR/usr/share/themes/* /usr/share/themes/ -r
-echo "[2/5] Installing icons and cursors..."
-cp $SCRIPT_DIR/usr/share/icons/* /usr/share/icons/ -r
-echo "[3/5] Installing sounds..."
-cp $SCRIPT_DIR/usr/share/sounds/* /usr/share/sounds/ -r
-echo "[4/5] Installing backgrounds..."
-cp $SCRIPT_DIR/usr/share/backgrounds/* /usr/share/backgrounds/ -r
-echo "[5/5] Installing startup sound..."
-cp $SCRIPT_DIR/etc/xdg/autostart/* /etc/xdg/autostart/ -r
-echo "Done!"
+installation() {
+  echo "[1/5] Installing themes..."
+  cp $SCRIPT_DIR/usr/share/themes/* /usr/share/themes/ -r
+  echo "[2/5] Installing icons and cursors..."
+  cp $SCRIPT_DIR/usr/share/icons/* /usr/share/icons/ -r
+  echo "[3/5] Installing sounds..."
+  cp $SCRIPT_DIR/usr/share/sounds/* /usr/share/sounds/ -r
+  echo "[4/5] Installing backgrounds..."
+  cp $SCRIPT_DIR/usr/share/backgrounds/* /usr/share/backgrounds/ -r
+  echo "[5/5] Installing startup sound..."
+  cp $SCRIPT_DIR/etc/xdg/autostart/* /etc/xdg/autostart/ -r
+}
+
+error() {
+  echo "The installation of Ambiant-Mate-NG was incomplete!"
+  exit 1
+}
+
+installation || error
 exit
